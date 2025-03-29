f6e671703d26ed70bae89d8139cf80e4b7779d1c
RockyAtoyan <111198613+RockyAtoyan@users.noreply.github.com>
2025-03-29 16:22:56 +0300

    feat: setup testing enviroment

--- server/src/db/db.service.ts
+++ server/src/db/db.service.ts
@@ -6,4 +6,18 @@ export class DbService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
   }
+
+  async resetTestDatabase() {
+    if (process.env.NODE_ENV !== 'test') return;
+
+    const models = Reflect.ownKeys(this).filter(
+      (key) => key[0] !== '_' && key[0] !== '$',
+    );
+
+    await Promise.all(
+      models.map(
+        (modelKey) => this[modelKey]?.deleteMany && this[modelKey].deleteMany(),
+      ),
+    );
+  }
 }
--- server/src/tags/tags.service.spec.ts
+++ server/src/tags/tags.service.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { DbService } from 'src/db/db.service';
+import { CreateTagDto } from './dto/create-tag.dto';
 import { TagsService } from './tags.service';
 
 describe('TagsService', () => {
   let service: TagsService;
+  let dbService: DbService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TagsService],
+      providers: [DbService, TagsService],
     }).compile();
 
+    dbService = module.get<DbService>(DbService);
     service = module.get<TagsService>(TagsService);
+    await dbService.resetTestDatabase();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be created', async () => {
+    const dto: CreateTagDto = { name: 'created-tag' };
+    const tag = await service.create(dto);
+    console.log(tag);
+    expect(tag.name).toEqual(dto.name);
   });
 });
